f7f1244266ff5b558fa19005a5fa0dfb6c1cf36f
Matheus Medeiros <matheussmedeiros7355@gmail.com>
2022-07-23 19:45:45 -0300

    run build

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.sass'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
       <h1>Hello world</h1>
       <Sidebar />
       <MainContainer />
--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -13,35 +13,35 @@ const TechnologiesContainer = () => {
     <h2>Tecnologias</h2>
     <div className={styles.technology_grid}>
       <div className={styles.technology_card_html}>
-        <i class="fa-brands fa-html5"></i>
+        <i className="fa-brands fa-html5"></i>
           <div>
             <h3>HTML5</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
       </div>
       <div className={styles.technology_card_css}>
-      <i class="fa-brands fa-css3"></i>
+      <i className="fa-brands fa-css3"></i>
           <div>
             <h3>CSS3</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
       </div>
       <div className={styles.technology_card_js}>
-      <i class="fa-brands fa-js"></i>
+      <i className="fa-brands fa-js"></i>
           <div>
             <h3>JavaScript</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
       </div>
       <div className={styles.technology_card_react}>
-      <i class="fa-brands fa-react"></i>
+      <i className="fa-brands fa-react"></i>
           <div>
             <h3>React</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
       </div>
       <div className={styles.technology_card_sass}>
-      <i class="fa-brands fa-sass"></i>
+      <i className="fa-brands fa-sass"></i>
           <div>
             <h3>SASS</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>

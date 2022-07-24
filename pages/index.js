9c582e9dc90ed27d54b2a4112f650a46d96a6810
Matheus Medeiros <matheussmedeiros7355@gmail.com>
2022-07-24 15:27:23 -0300

    Desining att

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
-      <h1>Hello world</h1>
+      <h1>Matheus Medeiros</h1>
       <Sidebar />
       <MainContainer />
     </div>

1b3ffb9f5976f3b0b151d257af487181929ab636
Matheus Medeiros <matheussmedeiros7355@gmail.com>
2022-07-24 15:41:54 -0300

    run build

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -2,9 +2,9 @@ import styles from '../../styles/socialnetworks.module.sass'
 
 const SocialNetworks = () => {
   return <section className={styles.social_networks}>
-        <a className={styles.social_btn_instagram} href="https://www.instagram.com/_math_abacaxi/" target="_blank"><i className="fa-brands fa-instagram"></i></a>
-        <a className={styles.social_btn_github} href="https://github.com/MatheussMedeiros" target="_blank"><i className="fa-brands fa-github-square"></i></a>
-        <a className={styles.social_btn_linkedin} href="https://www.linkedin.com/in/matheus-medeiros-653081246/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
+        <a className={styles.social_btn_instagram} href="https://www.instagram.com/_math_abacaxi/" ><i className="fa-brands fa-instagram"></i></a>
+        <a className={styles.social_btn_github} href="https://github.com/MatheussMedeiros" ><i className="fa-brands fa-github-square"></i></a>
+        <a className={styles.social_btn_linkedin} href="https://www.linkedin.com/in/matheus-medeiros-653081246/" ><i className="fa-brands fa-linkedin"></i></a>
     </section>
   
 }
